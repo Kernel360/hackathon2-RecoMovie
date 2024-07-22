0658beea50e034312d2a477c2562c6de80e29e17
I-migi <sunn10189@gmail.com>
2024-07-22 15:48:43 +0900

    html 폼 만드는 중

--- src/main/java/com/hackathonteam2/recomovie/user/controller/UserController.java
+++ src/main/java/com/hackathonteam2/recomovie/user/controller/UserController.java
@@ -4,12 +4,10 @@ import com.hackathonteam2.recomovie.user.dto.LoginRequest;
 import com.hackathonteam2.recomovie.user.entity.User;
 import com.hackathonteam2.recomovie.user.repository.UserRepository;
 import com.hackathonteam2.recomovie.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,7 +41,24 @@ public class UserController {
         } else{
             return "redirect:/register?error";
         }
+    }
 
+    @PostMapping("/login")
+    public String login(@RequestParam String loginId, @RequestParam String password, HttpSession httpSession){
+        boolean isLogin = userService.loginService(loginId,password);
+        if(isLogin){
+            User user = userService.getUserByLoginId(loginId);
+            httpSession.setAttribute("loggedInUser",user);
+            return "redirect:/home";
+        }else{
+            return "redirect:/login?error";
+        }
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession){
+        httpSession.removeAttribute("loggedInUser");
+        return "redirect:/login";
     }
 
 
