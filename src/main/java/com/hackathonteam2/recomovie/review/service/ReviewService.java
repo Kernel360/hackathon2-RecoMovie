8073dec52ccbe0b8ca6e03e9f9dcc254d94721fa
이선우 <117967333+I-migi@users.noreply.github.com>
2024-07-22 18:57:16 +0900

    홈화면에서 리뷰 리스트 보이기 (#12)
    * 회원가입 기능 만들었습니다.
    
    * html 폼 만드는 중
    
    * html 폼 만드는 중
    
    * 로그인 회원가입 기능,html 수정 완료
    
    * 로그인 회원가입 기능,html 수정 완료
    
    * 리뷰 작성 시 DB에 저장 기능 구현 + 리뷰 페이지 초안 제작
    
    * 홈 화면에서 리뷰 목록 보이기

--- src/main/java/com/hackathonteam2/recomovie/review/service/ReviewService.java
+++ src/main/java/com/hackathonteam2/recomovie/review/service/ReviewService.java
@@ -8,11 +8,17 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
     private final UserRepository userRepository;
+
+    public List<Review> getAllReviews(){
+        return reviewRepository.findAll();
+    }
 
     @Transactional
     public void writeReview(String longId, String content, int rating){
