9dfc377b7f137eb40c14d9e40ecd4e0910f89f68
gooot <kjd99002@naver.com>
2024-07-22 12:23:27 +0900

    init

---
+++ src/main/java/com/hackathonteam2/recomovie/RecoMovieApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonteam2.recomovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecoMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathonteam2/recomovie/RecoMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonteam2.recomovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
