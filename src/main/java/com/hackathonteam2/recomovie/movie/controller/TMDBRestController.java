0c1d994c57ddbd9a215b36c8cd39364c3981ba91
song <shd2768@naver.com>
2024-07-24 22:06:21 +0900

    fix : [#41] TMDB Api를 통해 현재 상영 영화 리스트 조회 및 영화 상세정보 조회
    - Api를 통해 현재 상영 영화 리스트를 조회
    - movie_id 값을 받아 Api를 통해 영화 상세정보 조회

--- src/main/java/com/hackathonteam2/recomovie/movie/controller/TMDBRestController.java
+++ src/main/java/com/hackathonteam2/recomovie/movie/controller/TMDBRestController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hackathonteam2.recomovie.movie.dto.NowPlayingResponse;
 import com.hackathonteam2.recomovie.movie.dto.TMDBDetailsDto;
-import com.hackathonteam2.recomovie.movie.service.MovieService;
 import com.hackathonteam2.recomovie.movie.service.TMDBService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,21 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 public class TMDBRestController {
 
 	private final TMDBService tmdbService;
-	private final MovieService movieService;
-
-	// 기존 search 기능 사용 안함으로 주석 처리 (추후 삭제 필요)
-	// @GetMapping("/search")
-	// public  List<MovieResponseDto> search(@RequestParam(name = "keyword") String keyword) throws JsonProcessingException {
-	//     List<TMDBMovieResponseDto> list = tmdbService.getByKeyword(keyword);
-	//     return list.stream()
-	//             .map(movieService::addMovieFromTMDB)
-	//             .toList();
-	// }
-
-	// @GetMapping("/get-all-data")
-	// public String getAllData() throws JsonProcessingException {
-	// 	return tmdbService.getByPeriod("2024-01-15", "2024-01-15").toString();
-	// }
 
 	@GetMapping("/nowPlaying")
 	public String getNowPlaying(@RequestParam int pageNum) throws JsonProcessingException {
